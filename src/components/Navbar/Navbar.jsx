e4c688680e72a1db580084ffc0916743586d6705
ChiChan17 <114010974+ChiChan17@users.noreply.github.com>
2023-10-17 21:32:28 -0700

    Commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,6 @@ const Navbar = () => {
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
-          Chi Chan
         </motion.span>
         <div className="social">
           <a href="#">
