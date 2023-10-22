fbe2bfae626cf5c9b269f8a5426e7fe577dce9d9
Taz Larson <tazlarson1@gmail.com>
2023-10-22 15:11:30 -0700

    finished login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from '../components/login/Login';
 import { Link } from 'react-router-dom';
 
 
+
+
 const LoginPage = () => {
+
+  
   return (
     <div className='Login'>
-        <Login/>
+      <Login/>
     </div>
   );
 };
